4502f9e0047ebb57fdf045fc79d5829c51c8477e
maxisusi <maxbalej@proton.me>
2025-01-03 21:42:51 +0100

    Translate object with displacement

--- www/src/index.ts
+++ www/src/index.ts
@@ -24,56 +24,48 @@ canvas.width = C_WIDTH;
 canvas.height = C_HEIGHT;
 
 const ctx = canvas.getContext("2d");
+
 if (!ctx) {
   throw new Error("2D Context not supported from this browser");
 }
 
 let dragStart: number[] = [];
+let initialBlockPosition = [100, 100];
+let currentBlockPosition = [...initialBlockPosition];
 
-canvas.addEventListener("dragstart", (event) => {
-  const img = new Image();
-  img.src =
-    "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
-  event.dataTransfer?.setDragImage(img, 0, 0);
-
-  dragStart = [event.clientX, event.clientY];
-});
-
-let currentBlock = [100, 100];
-
-$dbgBlock!.innerHTML = `X: ${currentBlock[0]} | Y: ${currentBlock[1]}`;
-
-const drawBackground = () => {
+const render = (x?: number, y?: number) => {
   // Draw background
   ctx.fillStyle = "#2e242c";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  drawDragLine();
+  if (x && y) drawLine(x, y);
 
   ctx.fillStyle = "#777";
-  ctx.fillRect(currentBlock[0], currentBlock[1], 50, 50);
+  ctx.fillRect(currentBlockPosition[0], currentBlockPosition[1], 50, 50);
 };
 
-const drawDragLine = () => {
-  // Draw drag line
+const drawLine = (x: number, y: number) => {
   if (dragStart.length !== 0) {
     ctx.fillStyle = "#777";
     ctx.fillRect(dragStart[0] - 5, dragStart[1] - 5, 10, 10);
     $dragStart!.innerHTML = `X: ${dragStart[0]} | Y: ${dragStart[1]}`;
+
+    ctx.beginPath();
+    ctx.strokeStyle = "#777";
+    ctx.moveTo(dragStart[0], dragStart[1]);
+    ctx.lineTo(x, y);
+    ctx.stroke();
   }
 };
 
-// Initial draw
-drawBackground();
+render();
+$dbgBlock!.innerHTML = `X: ${currentBlockPosition[0]} | Y: ${currentBlockPosition[1]}`;
 
 canvas.addEventListener("mousemove", (event) => {
   $mousePosition!.innerHTML = `Client X: ${event.clientX} | Client Y: ${event.clientY}`;
 });
 
-let prevDisplacement = [0, 0];
-
 canvas.addEventListener("dragover", (event) => {
-  console.log("dragover trigger");
   const { clientX, clientY } = event;
 
   const magnitude = Math.sqrt(
@@ -82,40 +74,39 @@ canvas.addEventListener("dragover", (event) => {
 
   const displacement = [clientX - dragStart[0], clientY - dragStart[1]];
 
-  let curDisplacement = [
-    displacement[0] - prevDisplacement[0],
-    displacement[1] - prevDisplacement[1],
-  ];
-
-  console.log(curDisplacement);
-
   $magnitude!.innerHTML = `${magnitude}`;
   $mousePosition!.innerHTML = `Client X: ${clientX} | Client Y: ${clientY}`;
   $displacement!.innerHTML = `X: ${displacement[0]} | Y: ${displacement[1]}`;
-  $dbgBlock!.innerHTML = `X: ${currentBlock[0]} | Y: ${currentBlock[1]}`;
+  $dbgBlock!.innerHTML = `X: ${currentBlockPosition[0].toFixed(2)} | Y: ${currentBlockPosition[1].toFixed(2)}`;
 
-  currentBlock = [
-    currentBlock[0] + displacement[0] / 100,
-    currentBlock[1] + displacement[1] / 100,
+  currentBlockPosition = [
+    initialBlockPosition[0] + displacement[0],
+    initialBlockPosition[1] + displacement[1],
   ];
 
-  drawBackground();
-
-  ctx.beginPath();
-  ctx.strokeStyle = "#777";
-  ctx.moveTo(dragStart[0], dragStart[1]);
-  ctx.lineTo(clientX, clientY);
-  ctx.stroke();
+  render();
 });
 
 canvas.addEventListener("dragend", (event) => {
+  // const { clientX, clientY } = event;
+
   dragStart = [];
+  initialBlockPosition = currentBlockPosition;
 
   $magnitude!.innerHTML = `NULL`;
   $dragStart!.innerHTML = `NULL`;
   $displacement!.innerHTML = `NULL`;
 
-  $dbgBlock!.innerHTML = `X: ${currentBlock[0]} | Y: ${currentBlock[1]}`;
+  $dbgBlock!.innerHTML = `X: ${currentBlockPosition[0].toFixed(2)} | Y: ${currentBlockPosition[1].toFixed(2)}`;
 
-  drawBackground();
+  render();
+});
+
+canvas.addEventListener("dragstart", (event) => {
+  dragStart = [event.clientX, event.clientY];
+
+  const img = new Image();
+  img.src =
+    "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
+  event.dataTransfer?.setDragImage(img, 0, 0);
 });
