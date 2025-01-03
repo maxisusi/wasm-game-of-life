30487a6fd168c6d927dbf91c56a1bf7caee9aba4
maxisusi <maxbalej@proton.me>
2025-01-03 01:15:16 +0100

    wip

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,4 +1,4 @@
-import init, { Game } from "wasm-test";
+import init from "wasm-test";
 
 await init();
 
@@ -6,13 +6,17 @@ const canvas = document.getElementById(
   "game-canvas",
 ) as HTMLCanvasElement | null;
 
+// Debugger
+const $mousePosition = document.getElementById("mouse-position");
+const $dragStart = document.getElementById("drag-start");
+const $magnitude = document.getElementById("magnitude");
+
 if (!canvas) {
   throw new Error("Game canvas undefined");
 }
 
 const C_WIDTH = window.innerWidth;
 const C_HEIGHT = window.innerWidth;
-const CELL_SIZE = 32;
 
 canvas.width = C_WIDTH;
 canvas.height = C_HEIGHT;
@@ -22,22 +26,59 @@ if (!ctx) {
   throw new Error("2D Context not supported from this browser");
 }
 
-ctx.fillStyle = "#2e242c";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+let dragStart: number[] = [];
 
-const gm = Game.new(64);
+canvas.addEventListener("dragstart", (event) => {
+  const img = new Image();
+  img.src =
+    "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
+  event.dataTransfer?.setDragImage(img, 0, 0);
 
-for (let i = 0; i < C_WIDTH; ++i) {
+  dragStart = [event.clientX, event.clientY];
+});
+
+const drawBackground = () => {
+  ctx.fillStyle = "#2e242c";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  if (dragStart.length !== 0) {
+    ctx.fillStyle = "#777";
+    ctx.fillRect(dragStart[0] - 5, dragStart[1] - 5, 10, 10);
+    $dragStart!.innerHTML = `X: ${dragStart[0]} | Y: ${dragStart[1]}`;
+  }
+};
+
+// Initial draw
+drawBackground();
+
+canvas.addEventListener("mousemove", (event) => {
+  $mousePosition!.innerHTML = `Client X: ${event.clientX} | Client Y: ${event.clientY}`;
+});
+
+canvas.addEventListener("dragover", (event) => {
+  const { clientX, clientY } = event;
+
+  const magnitude = Math.sqrt(
+    Math.pow(clientX - dragStart[0], 2) + Math.pow(clientY - dragStart[1], 2),
+  );
+
+  $magnitude!.innerHTML = `${magnitude}`;
+  $mousePosition!.innerHTML = `Client X: ${event.clientX} | Client Y: ${event.clientY}`;
+
+  drawBackground();
+
   ctx.beginPath();
-  ctx.strokeStyle = "#605052";
-  ctx.moveTo(i * CELL_SIZE, 0);
-  ctx.lineTo(i * CELL_SIZE, C_HEIGHT);
+  ctx.strokeStyle = "#777";
+  ctx.moveTo(dragStart[0], dragStart[1]);
+  ctx.lineTo(clientX, clientY);
   ctx.stroke();
+});
 
-  ctx.beginPath();
+canvas.addEventListener("dragend", () => {
+  dragStart = [];
 
-  ctx.strokeStyle = "#605052";
-  ctx.moveTo(0, i * CELL_SIZE);
-  ctx.lineTo(C_HEIGHT, i * CELL_SIZE);
-  ctx.stroke();
-}
+  $magnitude!.innerHTML = `NULL`;
+  $dragStart!.innerHTML = `NULL`;
+
+  drawBackground();
+});
