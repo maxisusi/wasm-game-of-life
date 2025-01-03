2754f062df07050be4f355b39eefa853f243c7ec
maxisusi <maxbalej@proton.me>
2025-01-03 21:10:27 +0100

    wip - building up debug sys

--- www/src/index.ts
+++ www/src/index.ts
@@ -10,6 +10,8 @@ const canvas = document.getElementById(
 const $mousePosition = document.getElementById("mouse-position");
 const $dragStart = document.getElementById("drag-start");
 const $magnitude = document.getElementById("magnitude");
+const $displacement = document.getElementById("displacement-vector");
+const $dbgBlock = document.getElementById("debug-block");
 
 if (!canvas) {
   throw new Error("Game canvas undefined");
@@ -37,10 +39,23 @@ canvas.addEventListener("dragstart", (event) => {
   dragStart = [event.clientX, event.clientY];
 });
 
+let currentBlock = [100, 100];
+
+$dbgBlock!.innerHTML = `X: ${currentBlock[0]} | Y: ${currentBlock[1]}`;
+
 const drawBackground = () => {
+  // Draw background
   ctx.fillStyle = "#2e242c";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  drawDragLine();
+
+  ctx.fillStyle = "#777";
+  ctx.fillRect(currentBlock[0], currentBlock[1], 50, 50);
+};
+
+const drawDragLine = () => {
+  // Draw drag line
   if (dragStart.length !== 0) {
     ctx.fillStyle = "#777";
     ctx.fillRect(dragStart[0] - 5, dragStart[1] - 5, 10, 10);
@@ -55,15 +70,34 @@ canvas.addEventListener("mousemove", (event) => {
   $mousePosition!.innerHTML = `Client X: ${event.clientX} | Client Y: ${event.clientY}`;
 });
 
+let prevDisplacement = [0, 0];
+
 canvas.addEventListener("dragover", (event) => {
+  console.log("dragover trigger");
   const { clientX, clientY } = event;
 
   const magnitude = Math.sqrt(
     Math.pow(clientX - dragStart[0], 2) + Math.pow(clientY - dragStart[1], 2),
   );
 
+  const displacement = [clientX - dragStart[0], clientY - dragStart[1]];
+
+  let curDisplacement = [
+    displacement[0] - prevDisplacement[0],
+    displacement[1] - prevDisplacement[1],
+  ];
+
+  console.log(curDisplacement);
+
   $magnitude!.innerHTML = `${magnitude}`;
-  $mousePosition!.innerHTML = `Client X: ${event.clientX} | Client Y: ${event.clientY}`;
+  $mousePosition!.innerHTML = `Client X: ${clientX} | Client Y: ${clientY}`;
+  $displacement!.innerHTML = `X: ${displacement[0]} | Y: ${displacement[1]}`;
+  $dbgBlock!.innerHTML = `X: ${currentBlock[0]} | Y: ${currentBlock[1]}`;
+
+  currentBlock = [
+    currentBlock[0] + displacement[0] / 100,
+    currentBlock[1] + displacement[1] / 100,
+  ];
 
   drawBackground();
 
@@ -74,11 +108,14 @@ canvas.addEventListener("dragover", (event) => {
   ctx.stroke();
 });
 
-canvas.addEventListener("dragend", () => {
+canvas.addEventListener("dragend", (event) => {
   dragStart = [];
 
   $magnitude!.innerHTML = `NULL`;
   $dragStart!.innerHTML = `NULL`;
+  $displacement!.innerHTML = `NULL`;
+
+  $dbgBlock!.innerHTML = `X: ${currentBlock[0]} | Y: ${currentBlock[1]}`;
 
   drawBackground();
 });
