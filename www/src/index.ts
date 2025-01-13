431427396ffe9db11b4489b155d698d968ae0398
maxisusi <maxbalej@proton.me>
2025-01-13 06:41:10 +0100

    wip displaying grid

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,20 +1,9 @@
-import init, { Game, Cell } from "wasm-test";
+import init, { Game } from "wasm-test";
 import { Block, Vector2d } from "./vector";
-
-const wasm = await init();
-
-const game = Game.new(8);
-
-// game.tick();
-
-// console.log(wasm.memory.buffer);
-const cells = new Uint8Array(wasm.memory.buffer, game.get_array(), 64);
-// console.log(cells);
 
 const canvas = document.getElementById(
   "game-canvas",
 ) as HTMLCanvasElement | null;
-
 // Debugger
 const $mousePosition = document.getElementById("mouse-position");
 const $zoom = document.getElementById("zoom");
@@ -23,6 +12,10 @@ if (!canvas) {
   throw new Error("Game canvas undefined");
 }
 
+const wasm = await init();
+const game = Game.new(8);
+
+const cells = new Uint8Array(wasm.memory.buffer, game.get_array(), 64);
 const C_WIDTH = window.innerWidth;
 const C_HEIGHT = window.innerWidth;
 
@@ -50,8 +43,8 @@ const render = () => {
 
   const blockList = Array.from(cells).map((c, i) => {
     return new Block(
-      100 * (i + 1),
-      i > 8 ? 200 : 100,
+      100 * (i % 8),
+      100 * Math.floor(i / 8),
       50,
       50,
       ctx,
@@ -67,7 +60,9 @@ const renderGame = () => {
   game.tick();
   render();
 
-  requestAnimationFrame(renderGame);
+  setTimeout(() => {
+    requestAnimationFrame(renderGame);
+  }, 1000 / 5);
 };
 
 renderGame();
